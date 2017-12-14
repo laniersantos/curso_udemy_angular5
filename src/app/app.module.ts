ee5ec79fea9532ff1dcb954929af90db1db3daf1
Lanier Santos <lanier.s@sidiamao.com.br>
2017-12-14 17:19:58 -0400

    Criacao de componentes para construcao da tela do sistema de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { AppServerComponent } from './app-server/app-server.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { InputTwoWayDatabindComponent } from './input-two-way-databind/input-two-way-databind.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping--edit/shopping--edit.component';
 
 
 @NgModule({
@@ -15,9 +22,16 @@ import { InputTwoWayDatabindComponent } from './input-two-way-databind/input-two
     AppComponent,
     AppServerComponent,
     AppServersComponent,
+    HeaderComponent,
     SuccessAlertComponent,
     WarningAlertComponent,
-    InputTwoWayDatabindComponent
+    InputTwoWayDatabindComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
