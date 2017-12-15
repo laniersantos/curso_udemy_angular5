74adc697cfb95740029f22835be0aa25cd458b3b
Lanier Santos <lanier.s@sidiamao.com.br>
2017-12-15 18:02:06 -0400

    Implementacao e exibicao de RecipesComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = new Array(...[new Recipe('A Test Recipe', 'This is simply a test', 'http://www.seriouseats.com/images/2017/04/20170405-one-pot-chicken-recipes-roundup-collage.jpg'),
+                                         new Recipe('A Test Recipe', 'This is simply a test', 'http://www.seriouseats.com/images/2017/04/20170405-one-pot-chicken-recipes-roundup-collage.jpg')]);
 
   constructor() { }
 
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-shopping--edit',
-  templateUrl: './shopping--edit.component.html',
-  styleUrls: ['./shopping--edit.component.css']
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
 
